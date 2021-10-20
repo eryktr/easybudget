765e03cd3669935a82fe81f1b05eae2d1f5fcf55
eryktr <eryk.trzeciakiewicz@gmail.com>
2021-10-20 17:24:37 +0200

    Implemented auxilary decorator to cater for jwt

--- backend/easybudget/routes.py
+++ backend/easybudget/routes.py
@@ -1,9 +1,22 @@
+import math
 from http import HTTPStatus
+from typing import Callable, Any
 
 import easybudget.hashing as hashing
 import easybudget.jwt_provider as jwt_provider
 from easybudget.db.db import DbService
-from itertools import chain
+
+
+def require_jwt(f):
+    def inner(db_service, request, jwt_secret):
+        token = _fetch_token(request)
+        payload = jwt_provider.decode(token, jwt_secret)
+        if payload is None:
+            return {'status': 'Invalid token'}, HTTPStatus.BAD_REQUEST
+        request.jwt_payload = payload
+        return f(db_service, request, jwt_secret)
+
+    return inner
 
 
 def register(db_service: DbService, request) -> tuple[dict, int]:
@@ -37,23 +50,22 @@ def get_users(db_service: DbService) -> dict[str, list]:
     return {'users': [u.serialize() for u in users]}
 
 
+@require_jwt
 def get_budgets(db_service: DbService, request, jwt_secret: str) -> tuple[dict, int]:
-    token = _fetch_token(request)
-    payload = jwt_provider.decode(token, jwt_secret)
-    if payload is None:
-        return {'status': 'Invalid token'}, HTTPStatus.BAD_REQUEST
-    budgets = db_service.budgets_of(payload['username'])
-    collaborates = db_service.budgets_shared_with(payload['username'])
-    return {'budgets': [b.serialize() for b in chain(budgets, collaborates)]}, 200
+    page = int(request.args.get('page') or 0)
+    items_per_page = 3
+    type_ = request.args['type']
+    getter: Callable[[str], Any] \
+        = db_service.budgets_of if type_ == 'own' else db_service.budgets_shared_with
+    budgets = getter(request.jwt_payload['username'])
+    num_pages = math.ceil(budgets.count() / items_per_page)
+    data = budgets.skip(page * items_per_page).limit(items_per_page)
+    return {'budgets': [b.serialize() for b in data], 'page': page, 'num_pages': num_pages}, 200
 
 
+@require_jwt
 def create_budget(db_service: DbService, request, jwt_secret: str):
-    token = _fetch_token(request)
-    payload = jwt_provider.decode(token, jwt_secret)
-    if payload is None:
-        return {'status': 'Invalid token.'}, HTTPStatus.UNAUTHORIZED
-
-    author = db_service.get_user(payload['username'])
+    author = db_service.get_user(request.jwt_payload['username'])
     amount = request.json['amount']
     name = request.json['name']
 
@@ -61,19 +73,8 @@ def create_budget(db_service: DbService, request, jwt_secret: str):
     return budget.serialize(), 200
 
 
-def get_budget(db_service: DbService, request, jwt_secret: str):
-    token = _fetch_token(request)
-    payload = jwt_provider.decode(token, jwt_secret)
-    if payload is None:
-        return {'status': 'Invalid token.'}, HTTPStatus.UNAUTHORIZED
-
-
+@require_jwt
 def patch_budget(db_service: DbService, request, jwt_secret: str):
-    token = _fetch_token(request)
-    payload = jwt_provider.decode(token, jwt_secret)
-    if payload is None:
-        return {'status': 'Invalid token.'}, HTTPStatus.UNAUTHORIZED
-
     budget_id = request.json['budget_id']
     budget = db_service.get_budget(budget_id)
     contributors_nicknames = request.json.get('contributors')
